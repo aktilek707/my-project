dfc79696eda95d0c2b1c52dff1ae0d3f3021c890
aktileka <baieldjumabaev@gmail.com>
2025-01-14 12:24:31 +0600

    added js

--- scrtpt.js
+++ scrtpt.js
@@ -3,3 +3,6 @@ let div = document.querySelector(".container")
 let button = document.querySelector(".content")
 
 let clous = document.querySelector(".clous")
+
+let button1 = document.querySelector(".button1")
+
